0f33e991750d6bfefcce9676aa34f84f7aeec25c
Bob Swift <bswift@rsds.ca>
2024-11-27 10:09:47 -0700

    Change tabs to spaces.

--- musicbrainzngs/compat.py
+++ musicbrainzngs/compat.py
@@ -38,24 +38,24 @@ is_py3 = (_ver[0] == 3)
 # ---------
 
 if is_py2:
-	from StringIO import StringIO
-	from urllib2 import HTTPPasswordMgr, HTTPDigestAuthHandler, Request,\
-						HTTPHandler, build_opener, HTTPError, URLError
-	from httplib import BadStatusLine, HTTPException
-	from urlparse import urlunparse
-	from urllib import urlencode, quote_plus
+    from StringIO import StringIO
+    from urllib2 import HTTPPasswordMgr, HTTPDigestAuthHandler, Request,\
+                        HTTPHandler, build_opener, HTTPError, URLError
+    from httplib import BadStatusLine, HTTPException
+    from urlparse import urlunparse
+    from urllib import urlencode, quote_plus
 
-	bytes = str
-	unicode = unicode
-	basestring = basestring
+    bytes = str
+    unicode = unicode
+    basestring = basestring
 elif is_py3:
-	from io import StringIO
-	from urllib.request import HTTPPasswordMgr, HTTPDigestAuthHandler, Request,\
-								HTTPHandler, build_opener
-	from urllib.error import HTTPError, URLError
-	from http.client import HTTPException, BadStatusLine
-	from urllib.parse import urlunparse, urlencode, quote_plus
+    from io import StringIO
+    from urllib.request import HTTPPasswordMgr, HTTPDigestAuthHandler, Request,\
+                                HTTPHandler, build_opener
+    from urllib.error import HTTPError, URLError
+    from http.client import HTTPException, BadStatusLine
+    from urllib.parse import urlunparse, urlencode, quote_plus
 
-	unicode = str
-	bytes = bytes
-	basestring = (str,bytes)
+    unicode = str
+    bytes = bytes
+    basestring = (str,bytes)
